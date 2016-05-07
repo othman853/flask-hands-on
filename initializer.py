30060a066b93869aec32ef2076e0ffe19795e223
othman853 <anuar.yasser@gmail.com>
2016-05-07 15:33:17 -0300

    Adding 404 custom page and error handler.

--- initializer.py
+++ initializer.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.j2'), 404
 
 from routes.salutations import *
