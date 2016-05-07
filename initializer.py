86d40534b5aaad32da5ecfe963dfff5ec9104fdf
othman853 <anuar.yasser@gmail.com>
2016-05-07 18:07:01 -0300

    Setting up Flask Login

--- initializer.py
+++ initializer.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask_login import LoginManager
+from models import User
 
 app = Flask(__name__)
+
+app.secret_key = 'flask_hands_on_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login.j2'
+
+@login_manager.user_loader
+def login_user_loader(user_id):
+    return User('name', 'password')
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.j2'), 404
 
+from routes.login import *
 from routes.salutations import *
