5dc95a7465564383e387f599613a66de57315746
othman853 <anuar.yasser@gmail.com>
2016-05-09 16:05:25 -0300

    Creating a route for registration of new users

--- routes/login.py
+++ routes/login.py
@@ -3,13 +3,32 @@ from flask import render_template, request, redirect, url_for
 from flask_login import login_user
 from models import User
 
-@app.route('/login', methods=['GET'])
-def get_login():
-    return render_template('login.j2')
+LOGIN_OPERATION = 'login'
+REGISTER_OPERATION = 'register'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'GET':
+        return render_template('login.j2', operation=REGISTER_OPERATION)
+
+    username = request.form['name']
+    password = request.form['password']
+
+    if User.is_username_taken(username):
+        return render_template('forbidden.j2')
+
+    user = User(username, password)
+    user.save()
+
+    return render_template('login.j2', operation=LOGIN_OPERATION)
 
 
-@app.route('/login', methods=['POST'])
-def post_login():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.j2', operation=LOGIN_OPERATION)
 
     username = request.form['name']
     password = request.form['password']
